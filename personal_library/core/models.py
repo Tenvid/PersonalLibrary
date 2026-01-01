b9218b58be2fb7afd3768d761583a76f4f73041b
David Gómez Barberá <dagobar.pk.db@gmail.com>
2026-01-01 13:10:23 +0100

    Add data models

--- personal_library/core/models.py
+++ personal_library/core/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+    biography = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=200)
+    country = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    publisher = models.ForeignKey(Publisher, on_delete=models.SET_NULL, blank=True, null=True)
+    synopsis = models.TextField()
+    publication_date = models.DateField()
+    cover_image = models.ImageField(upload_to='book_covers/', blank=True, null=True)
+
+    def __str__(self):
+        return self.title
