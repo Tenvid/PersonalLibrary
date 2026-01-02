be7de220633c6f969d281d62dee80169950e339f
David Gómez Barberá <dagobar.pk.db@gmail.com>
2026-01-02 16:24:20 +0100

    Add static folder to settings

--- personal_library/personal_library/settings.py
+++ personal_library/personal_library/settings.py
@@ -116,6 +116,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 # Media files (User uploads)
 # https://docs.djangoproject.com/en/6.0/topics/files/
 
