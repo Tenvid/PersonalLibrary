45c95eba9eb6da6465428ec6cc4d023b07fd5675
David Gómez Barberá <dagobar.pk.db@gmail.com>
2026-02-08 14:53:27 +0100

    feat(books): complete migration to class-based

--- personal_library/core/_views/books.py
+++ personal_library/core/_views/books.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from django.core.paginator import Paginator
+from django.shortcuts import get_object_or_404
 from django.urls import reverse
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import UpdateView
+from django.views.generic.edit import CreateView, UpdateView
 from core.models import Book, Author, Publisher
 from django.views.generic.list import ListView
 
@@ -37,39 +36,6 @@ class BookDetailView(DetailView):
         return get_object_or_404(Book, id=book_id)
 
 
-def index(request):
-    # Get the search query from the request
-    query = request.GET.get("q", "")
-
-    # Get all books or filter based on the search query
-    if query:
-        books_list = Book.objects.filter(title__icontains=query).select_related(
-            "author", "publisher"
-        )
-    else:
-        books_list = Book.objects.all().select_related("author", "publisher")
-
-    # Paginate the results (10 books per page)
-    paginator = Paginator(books_list, 9)
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
-
-    context = {
-        "books": page_obj,
-        "query": query,
-        "is_paginated": page_obj.has_other_pages(),
-        "page_obj": page_obj,
-    }
-
-    return render(request, "core/index.html", context)
-
-
-def book_detail(request, book_id):
-    book = get_object_or_404(Book, id=book_id)
-    context = {"book": book}
-    return render(request, "core/book_detail.html", context)
-
-
 class BookEditView(UpdateView):
     model = Book
     template_name = "core/book_edit.html"
@@ -93,75 +59,23 @@ class BookEditView(UpdateView):
         return reverse("book_detail", kwargs={"book_id": self.object.id})
 
 
-def book_edit(request, book_id=None):
-    if book_id:
-        book = get_object_or_404(Book, id=book_id)
-        is_new = False
-    else:
-        book = None
-        is_new = True
+class BookCreateView(CreateView):
+    model = Book
+    template_name = "core/book_edit.html"
+    fields = [
+        "title",
+        "synopsis",
+        "publication_date",
+        "author",
+        "publisher",
+        "cover_image",
+    ]
 
-    if request.method == "POST":
-        title = request.POST.get("title")
-        synopsis = request.POST.get("synopsis")
-        publication_date = request.POST.get("publication_date")
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["authors"] = Author.objects.all()
+        context["publishers"] = Publisher.objects.all()
+        return context
 
-        # Handle author
-        author_id = request.POST.get("author")
-        author = None
-        if author_id:
-            try:
-                author = Author.objects.get(id=author_id)
-            except Author.DoesNotExist:
-                author = None
-
-        # Handle publisher
-        publisher_id = request.POST.get("publisher")
-        publisher = None
-        if publisher_id:
-            try:
-                publisher = Publisher.objects.get(id=publisher_id)
-            except Publisher.DoesNotExist:
-                publisher = None
-
-        if book:
-            # Update existing book
-            book.title = title
-            book.synopsis = synopsis
-            book.publication_date = publication_date
-            book.author = author
-            book.publisher = publisher
-
-            # Handle cover image
-            if "cover_image" in request.FILES:
-                book.cover_image = request.FILES["cover_image"]
-        else:
-            # Create new book
-            book = Book.objects.create(
-                title=title,
-                synopsis=synopsis,
-                publication_date=publication_date,
-                author=author,
-                publisher=publisher,
-            )
-
-            # Handle cover image for new book
-            if "cover_image" in request.FILES:
-                book.cover_image = request.FILES["cover_image"]
-
-        book.save()
-        return redirect(
-            "book_detail", book_id=book.id
-        )  # Redirect to the book detail page
-
-    # For GET request, show the form
-    authors = Author.objects.all()
-    publishers = Publisher.objects.all()
-
-    context = {
-        "book": book,
-        "authors": authors,
-        "publishers": publishers,
-        "is_new": is_new,
-    }
-    return render(request, "core/book_edit.html", context)
+    def get_success_url(self):
+        return reverse("book_detail", kwargs={"book_id": self.object.id})
