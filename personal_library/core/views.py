ba19913f562e36d93d5b6a2f593a86a926441d4d
David Gómez Barberá <dagobar.pk.db@gmail.com>
2026-02-10 21:40:28 +0100

    refactor(views): remove unused view function and imports

--- personal_library/core/views.py
+++ personal_library/core/views.py
@@ -1,36 +1,4 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render
 
-from core._views import books as book_views  # noqa
 from core.api import authors, books, publishers  # noqa
-from core.models import Book
-
-
-def index(request):
-    # Get the search query from the request
-    query = request.GET.get("q", "")
-
-    # Get all books or filter based on the search query
-    if query:
-        books_list = Book.objects.filter(title__icontains=query).select_related(
-            "author", "publisher"
-        )
-    else:
-        books_list = Book.objects.all().select_related("author", "publisher")
-
-    # Paginate the results (10 books per page)
-    paginator = Paginator(books_list, 9)
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
-
-    context = {
-        "books": page_obj,
-        "query": query,
-        "is_paginated": page_obj.has_other_pages(),
-        "page_obj": page_obj,
-    }
-
-    return render(request, "core/index.html", context)
-
-
-# Create your views here.
