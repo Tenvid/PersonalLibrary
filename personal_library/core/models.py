97e68b5a8b82e2f550453c08e8f323f8eddd2a83
David Gómez Barberá <dagobar.pk.db@gmail.com>
2026-02-23 17:34:11 +0100

    feat(models): add rental model

--- personal_library/core/models.py
+++ personal_library/core/models.py
@@ -1,6 +1,8 @@
-from django.db import models
+from datetime import datetime, timedelta
+from enum import StrEnum
 
-# Create your models here.
+from django.contrib.auth.models import User
+from django.db import models
 
 
 class Author(models.Model):
@@ -50,4 +52,34 @@ class Book(models.Model):
             "synopsis": self.synopsis,
             "publication_date": self.publication_date,
             "cover_image": self.cover_image.url if self.cover_image else None,
+        }
+
+
+class RentalStatus(StrEnum):
+    RENTED = "RENTED"
+    ON_TIME = "ON_TIME"
+    LATE = "LATE"
+
+
+class Rental(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
+    book = models.ForeignKey(Book, on_delete=models.RESTRICT, null=False, blank=False)
+    rented_at = models.DateTimeField(auto_now_add=True, null=False, blank=False)
+    expected_return_date = models.DateTimeField(
+        blank=True, null=True, default=datetime.now() + timedelta(days=14)
+    )
+    returned_at = models.DateTimeField(blank=True, null=True)
+    status = models.CharField(
+        max_length=20,
+        choices=[(status.value, status.value) for status in RentalStatus],
+    )
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "book": self.book.to_dict() if self.book else None,
+            "rented_at": self.rented_at,
+            "returned_at": self.returned_at,
+            "status": self.status,
+            "user": self.user.username if self.user else None,
         }
